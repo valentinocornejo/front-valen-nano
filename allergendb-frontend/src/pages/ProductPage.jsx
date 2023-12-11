dd8e1a0791a96ace72e7a0289bdea5fcd0647e5e
valentinocornejo <101400293+valentinocornejo@users.noreply.github.com>
2023-12-11 17:30:03 -0300

    Update ProductPage.jsx

--- allergendb-frontend/src/pages/ProductPage.jsx
+++ allergendb-frontend/src/pages/ProductPage.jsx
@@ -1,20 +1,24 @@
-import "./styles/Pagproductos.css";
+import "./styles/pagproductos.css";
 import React, { useState } from "react";
 export const Productos = () => {
   const handlehomepageClick = () => {
-
     window.location.href = "/homepage";
   };
   return (
     <div className="PPcontent">
       <div className="PPRemarco">
         <h1 className="PPtuperfil">Tu Producto</h1> <hr className="hr1" />
-        <h2 className="PPh2">Producto:</h2> <br /> <hr className="hr1"/>
-        <h2 className="PPh2">Precio:</h2> <br /> <hr className="hr1"/>
+        <h2 className="PPh2">Producto:</h2> <br /> <hr className="hr1" />
+        <h2 className="PPh2">Precio:</h2> <br /> <hr className="hr1" />
         <h2 className="PPh2">Alergenos:</h2> <br />
       </div>
       <br />
-      <img src=".\src\pages\styles\akergu_2.png" alt="Logo" className="PPlogo-img" onClick={handlehomepageClick}/>
+      <img
+        src=".\src\pages\styles\akergu_2.png"
+        alt="Logo"
+        className="PPlogo-img"
+        onClick={handlehomepageClick}
+      />
     </div>
   );
 };
